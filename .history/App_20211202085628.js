48ac5b46ca5ef31a5bd3676fd5fda77eace99020
sehoon <gsh723@douzone.com>
2021-12-02 22:04:23 +0900

    first commit and initial setting

---
+++ .history/App_20211202085628.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import styled from 'styled-components/native'
+
+const 
+
+export default function App() {
+  return null
+}
---
+++ .history/readme_20211202085532.md
@@ -0,0 +1,6 @@
+# ANIMATION & INTERACTION
+
+## 1. [SET UP]
+
+1. npx create-react-native-app
+2. npm i styled-components
