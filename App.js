fd784dded67bf0b651d2b6c5a3825dae0f32b2c8
sehoon <gsh723@douzone.com>
2021-12-07 08:57:21 +0900

    learn start() is callback function, useRef and easing of timing's properties

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Animated, TouchableOpacity, Text } from "react-native";
+import { Easing } from "react-native-reanimated";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -16,19 +17,24 @@ const Box = styled.View`
 
 const AnimatedBox = Animated.createAnimatedComponent(Box); // 2
 export default function App() {
-  const y = new Animated.Value(0); // 1
+  const [up, setUp] = useState(true);
+  const toggleUp = () => {
+    setUp((prev) => !prev);
+  };
+  // useRef : 다시 렌더링 일어나도 value를 유지할게 하는 HOOK
+  const y = useRef(new Animated.Value(0)).current; // 1
 
   const moveUp = () => {
-    Animated.spring(y, {
-      toValue: -200,
-      bounciness: 50,
-      speed: 50,
+    Animated.timing(y, {
+      toValue: up ? 200 : -200,
       useNativeDriver: true,
-    }).start();
+      easing: Easing.cubic,
+    }).start(toggleUp); // start function is callback function. that is the event arise when animation ends
   };
 
   // [y의 값을 얻고 싶을 때]
-  y.addListener(() => console.log(y));
+  y.addListener(() => console.log("Animated State : ", y));
+  console.log("Component State:", y);
   return (
     <Container>
       <TouchableOpacity onPress={moveUp}>
--- readme.md
+++ readme.md
@@ -17,8 +17,22 @@
 
 - 1. toValue
 - 2. useNativeDriver
+- #### 3. easing : Easing.elastic(5) : 여러가지 애니메이션을 첨가할 수 있다.
 
 ### 3) spring
 
 - 1. tension & friction(마찰)
 - 2. bouciness & speed
+
+### 4) start 🚀
+
+- Animated.timing(y, toValue : x, useNativeDriver : x).start()
+
+: start fuction is 'CALLBACK'! 그래서 애니메이션이 끝난 후 메소드를 구현한다.
+
+## 3. [UseRef] 🚀
+
+: React에서 useState로 인해 state값이 변할 때, 전체적으로 '재렌더링'이 일어남
+: 하지만 값을 유지하고 싶을 때! useRef를 이용
+
+- const y = useRef(new Animated.Value(0)).current; // 1
